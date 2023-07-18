30b77c226534c8c9c6d2c0031d9700f3fb57ba1c
SanjayNithin2002 <sanjaynithin.s2020@vitstudent.ac.in>
2023-07-18 14:57:31 +0530

    style changes

--- main.py
+++ main.py
@@ -31,9 +31,17 @@ async def on_message(message):
     if message.content.startswith("wiki"):
         a = message.content[4:]
         result = await run_in_thread(wiki, a)  # Use the alternative run_in_thread function
-        a = a.title()
-        embed = discord.Embed(title=a, description=result, color=0x00CED1)
-        await message.channel.send(embed=embed)
+
+        if result: 
+           await message.add_reaction("👍")
+           a = a.title()
+           embed = discord.Embed(title=a, description=result, color=0xC8E6C9)
+           await message.channel.send(embed=embed)
+           
+        else:
+           await message.add_reaction("👎")
+           embed = discord.Embed(title="Page Unavailable", description="Try different prompt.", color=0xFFCDD2)
+           await message.channel.send(embed=embed)
 
 async def run_in_thread(func, *args, **kwargs):
     with concurrent.futures.ThreadPoolExecutor() as executor:
